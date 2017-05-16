129c8ad230e507d02b1cf3d8c00727050b2d5a39
Vitali Novik <Vitali_Novik@epam.com>
2017-05-16 03:41:31 +0300

    Add project files.

---
+++ Application/CachingSolutionsSamples/CategoriesMemoryCache.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using NorthwindLibrary;
+using System.Runtime.Caching;
+
+namespace CachingSolutionsSamples
+{
+	internal class CategoriesMemoryCache : ICategoriesCache
+	{
+		ObjectCache cache = MemoryCache.Default;
+		string prefix  = "Cache_Categories";
+
+		public IEnumerable<Category> Get(string forUser)
+		{
+			return (IEnumerable<Category>) cache.Get(prefix + forUser);
+		}
+
+		public void Set(string forUser, IEnumerable<Category> categories)
+		{
+			cache.Set(prefix + forUser, categories, ObjectCache.InfiniteAbsoluteExpiration);
+		}
+	}
+}
